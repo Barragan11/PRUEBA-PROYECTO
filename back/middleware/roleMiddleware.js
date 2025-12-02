e3421754ea20910deeab4baa9789d61a4cd8c2d4
Luis Ángel Barragán González <158596646+Barragan11@users.noreply.github.com>
2025-12-01 19:19:27 -0600

    Delete back directory

--- back/middleware/roleMiddleware.js
+++
@@ -1,9 +0,0 @@
-// back/middleware/roleMiddleware.js
-function adminOnly(req, res, next) {
-  if (!req.user || req.user.role !== "admin") {
-    return res.status(403).json({ message: "Acceso solo para administradores" });
-  }
-  next();
-}
-
-module.exports = { adminOnly };
